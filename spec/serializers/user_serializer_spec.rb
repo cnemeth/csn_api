7c1636f9a957fbd253d0ed954746d004216164c6
Csaba Nemeth <csaba.nemeth@csabanemeth.org>
2016-06-28 15:15:23 -0700

    Added UserSerializer

---
+++ spec/serializers/user_serializer_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe UserSerializer, type: :serializer do
+
+  context 'Individual Resource Represantation' do
+    let(:resource) { create(:user) }
+    let(:serialization) { "#{resource.class}Serializer".constantize.new(resource) }
+    let(:serialized_resource) { JSON.parse(serialization.to_json)[resource.class.to_s.downcase] }
+
+    context "with valid attributes" do
+      it 'serializes the resource attributes' do
+        resource.class.attribute_names.each do | attribute |
+          klass = resource[attribute.to_sym].class
+          case klass
+          when Fixnum || TrueClass || FalseClass || NilClass
+            expect( serialized_resource[attribute] ).to eql( resource[attribute.to_sym] )
+          when ActiveSupport::TimeWithZone || DateTime
+            expect( serialized_resource[attribute] ).to include String(Date.today.year)
+          when String
+            expect( serialized_resource[attribute] ).to eql( String(resource[attribute.to_sym]) )
+          end
+        end
+      end
+    end
+
+    context "with invalid attribute" do
+      it 'returns nil' do
+        expect(resource['foo']).to be_nil
+      end
+    end
+  end
+end
+
