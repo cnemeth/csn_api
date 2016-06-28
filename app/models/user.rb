9857605e99da0fff0ecc46ef53afd7193643de1a
Csaba Nemeth <csaba.nemeth@csabanemeth.org>
2016-06-28 13:28:54 -0700

    Annotated User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  first_name :string
+#  last_name  :string
+#  email      :string
+#  phone      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class User < ApplicationRecord
   validates :first_name, :last_name, :email, :phone, presence: true
 end
