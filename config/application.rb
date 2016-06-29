3762a0f690b57b35e9e62276dfd2382b57240c36
Csaba Nemeth <csaba.nemeth@csabanemeth.org>
2016-06-28 17:08:19 -0700

    Configured Rack::Cors

--- config/application.rb
+++ config/application.rb
@@ -9,8 +9,6 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
-# require "sprockets/railtie"
-# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -30,6 +28,13 @@ module CsnApi
     config.generators do |g|
       g.test_framework :rspec, fixture: true
       g.fixture_replacement :factory_girl, dir: "spec/factories"
+    end
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :delete, :put, :options, :head]
+      end
     end
   end
 end
